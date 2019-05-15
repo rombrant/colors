cb4dce13a182fbf8f793905aa9d10fa44aded651
Roman Safin <gordonfreeman2332@gmail.com>
2019-05-15 18:47:05 +0300

    some changes components

--- ef0721a4e4657b2cc750.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{73:function(t,e,o){"use strict";o.r(e);var s=function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"h1"},[this._v("Portfolio")])};s._withStripped=!0;var i=o(2),n=Object(i.a)({},s,[],!1,null,null,null);n.options.__file="src/components/pages/portfolio.vue";e.default=n.exports}}]);
